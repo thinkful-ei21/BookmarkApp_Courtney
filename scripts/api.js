1ff0f986d1927af5a59a33b55400846e08ce7c3c
Courtney Adams <cadams8238@gmail.com>
2018-05-17 06:54:13 -0700

    initial commit

---
+++ scripts/api.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const api = (function() {
+
+	const API_URL; 
+
+	const fetchAllBookmarks = function() {
+
+	};
+
+	const updateBookmark = function() {
+
+	};
+
+	const createBookmark = function() {
+
+	};
+
+
+
+
+	return {
+		fetchAllBookmarks,
+		updateBookmark,
+		createBookmark
+	};
+}());
---
+++ scripts/localStore.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const localStore = (function() {
+
+
+	const addNewBookmark = function() {
+
+	};
+
+	const deleteBookmark = function() {
+
+	};
+
+	const updateBookmark = function() {
+
+	};
+
+	const searchByRating = function() {
+
+	};
+
+	const findById = function() {
+
+	};
+
+
+	return {
+		localBookmarks: [],
+		showRating: false,
+		adding: false,
+
+		addNewBookmark,
+		deleteBookmark,
+		updateBookmark,
+		searchByRating,
+		findById
+	};
+}());
